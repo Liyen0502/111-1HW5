483ad42d8f90f03d42e87733393cb03a2444f064
Liyen0502 <thekingadd0502@gmail.com>
2022-12-17 21:58:25 +0800

    0

--- 111-1HW5/WebForm1.aspx.cs
+++ 111-1HW5/WebForm1.aspx.cs
@@ -1,29 +1,18 @@
-﻿# 第5次作業-作業-HW5
->
->學號：109111104
-><br />
->姓名：李彥賓
-><br />
->作業撰寫時間：40 (mins，包含程式撰寫時間)
-><br />
->最後撰寫文件日期：2022/12/17
->
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
-本份文件包含以下主題：(至少需下面兩項，若是有多者可以自行新增)
-- [x]說明內容
-- [x]個人認為完成作業須具備觀念
-
-## 說明程式與內容
-
-開始寫說明，該說明需說明想法，
-並於之後再對上述想法的每一部分將程式進一步進行展現，
-若需引用程式區則使用下面方法，
-若為.cs檔內程式除了於敘述中需註明檔案名稱外，
-還需使用語法` ```csharp 程式碼 ``` `，
-下段程式碼則為使用後結果：
-
-```csharp
-protected void Page_Load(object sender, EventArgs e)
+namespace _111_1HW5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
         {
             string s_str = ConfigurationManager.ConnectionStrings["MSSQLLocalDB"].ConnectionString;
             if (!IsPostBack)
@@ -67,36 +56,5 @@ protected void Page_Load(object sender, EventArgs e)
                 Response.Write(o_e.ToString());
             }
         }
-```
-
-若要於內文中標示部分.aspx檔，則使用以下標籤` ```html 程式碼 ``` `，
-下段程式碼則為使用後結果：
-
-```html
-<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="_111_1HW5.WebForm1" %>
-
-<!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title></title>
-</head>
-<body>
-    <form id="form1" runat="server">
-        <div>
-            <asp:Button ID="btn_Insert" runat="server" Text="點我新增資料夾" OnClick="btn_Insert_Click" />
-            <asp:GridView ID="gd_View" runat="server"></asp:GridView>
-        </div>
-    </form>
-</body>
-</html>
-```
-
-
-## 個人認為完成作業須具備觀念
-
-開始寫說明，需要說明本次作業個人覺得需學會那些觀念 (需寫成文章，需最少50字，
-並且文內不得有你、我、他三種文字)
-
-學會建立資料庫，運用SQL Injection設定資料表的資料，用網路資料庫結合呈現資料表。
+    }
+ }
